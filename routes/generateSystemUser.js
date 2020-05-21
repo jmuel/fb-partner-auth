46587218ee89085337593ca50308df89541aee81
James Mueller <jmuel@fb.com>
2020-05-20 23:43:48 -0700

    set up basic flow to generate valid ssapi system user

---
+++ routes/generateSystemUser.js
@@ -0,0 +1,40 @@
+const bizSdk = require('facebook-nodejs-business-sdk');
+const crypto = require('crypto');
+
+const result = require('dotenv').config();
+
+const partnerBusinessAdminAccessToken = process.env.BUSINESS_ADMIN_ACCESS_TOKEN;
+const partnerBusinessId = process.env.BUSINESS_ID;
+const appSecret = process.env.APP_SECRET;
+const appId = process.env.APP_ID;
+
+const Business = bizSdk.Business;
+const User = bizSdk.User;
+const AdsPixel = bizSdk.AdsPixel;
+const SystemUser = bizSdk.SystemUser;
+const FacebookAdsApi = bizSdk.FacebookAdsApi;
+
+async function generateClientSystemUserAccessToken(clientAccessToken, pixelId) {
+    console.log(clientAccessToken);
+    console.log(pixelId);
+    const api = FacebookAdsApi.init(clientAccessToken);
+    api.setDebug(true);
+    const pixel = await new AdsPixel(pixelId).read([AdsPixel.Fields.owner_business]);
+    const pixelBusiness = await new Business(pixel.owner_business.id);
+    const result = await new Business(partnerBusinessId).createManagedBusiness(null, {existing_client_business_id: pixelBusiness.id});
+    FacebookAdsApi.init(partnerBusinessAdminAccessToken);
+    console.log("partner access token: ", partnerBusinessAdminAccessToken);
+    const accessTokenResponse = await new Business(pixelBusiness.id).createAccessToken(null, {
+        system_user_name: 'jamesttestuser1',
+        scope: 'ads_read',
+        app_id: appId,
+    });
+    const createdAccessToken = accessTokenResponse.access_token;
+    const suAPI = FacebookAdsApi.init(createdAccessToken);
+    const systemUser = await suAPI.call('GET', ['me']);
+    FacebookAdsApi.init(clientAccessToken);
+    const res = await new AdsPixel(pixelId).createAssignedUser(null, {tasks: ['EDIT'], user: systemUser.id})
+
+    return createdAccessToken;
+}
+module.exports = generateClientSystemUserAccessToken;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const generateSystemUser = require('./generateSystemUser.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'FB Partner Login Test App' });
 });
+
+router.get('/ssapiSetup', function(req, res, next) {
+  const clientToken = req.query.token;
+  const clientPixel = req.query.pixel;
+  generateSystemUser(clientToken, clientPixel).then(suAccessToken => res.json(suAccessToken));
+
+})
 
 module.exports = router;
