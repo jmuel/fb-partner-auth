02a964f5fafa78e4c64b6c64b9a73008daf06f68
James Mueller <jmuel@fb.com>
2020-05-21 10:26:21 -0700

    fixing for heroku

---
+++ public/javascripts/fblogin.js
@@ -0,0 +1,48 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+const appId = '543434039867366';
+const isDebugMode = true;
+const scopes =
+  'ads_management,ads_read,manage_business_extension,business_management';
+const jsVersion = 'v6.0';
+
+window.fbAsyncInit = function() {
+  // FB JavaScript SDK configuration and setup
+  FB.init({
+    appId: appId, // FB App ID
+    cookie: true, // enable cookies to allow the server to access the session
+    xfbml: false, // parse social plugins on this page
+    version: jsVersion, // use graph api version
+  });
+};
+
+// Load the JavaScript SDK asynchronously
+(function(d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = '//connect.facebook.net/en_US/sdk.js';
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, 'script', 'facebook-jssdk');
+
+// Facebook login with JavaScript SDK
+function fbLogin() {
+  FB.login(
+    function(response) {
+      console.log(response);
+      const accessToken = response.authResponse.accessToken;
+      const pixelID = document.getElementById("pixelID").value;
+      fetch(`https://localhost:3000/ssapiSetup?token=${accessToken}&pixel=${pixelID}`)
+      .then(response => response.json())
+        .then(response => {
+          console.log('access token', response);
+          const tokenNode = document.getElementById('tokenOut');
+          tokenNode.textContent = response;
+          });
+    },
+    {
+      scope: ['business_management','ads_management']
+    },
+  );
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const generateSystemUser = require('./generateSystemUser');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/ssapiSetup', function(req, res, next) {
+  const token = req.params.token;
+  const pixelId = req.params.pixel;
+  generateSystemUser(token, pixelId).then(resp => res.json(response));
 });
 
 module.exports = router;
